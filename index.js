3396d3f9da303501646983a1de4eb785377cb8a8
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2024-04-11 23:26:12 +0700

    การใช้ chalk

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
-// Link your project globally with
-// npm link
 import { program } from "commander";
+import chalk from "chalk";
 
 program
   .version("1.0.0")
   .description("My Node CLI")
   .option("-n, --name <type>", "Add your name")
   .action((options) => {
-    console.log(`Hey ${options.name}`);
+    console.log(chalk.blue(`Hey GG, ${options.name}`));
+    console.log(chalk.green(`Hey, ${options.name}`));
+    console.log(chalk.red(`Hey, ${options.name}`));
+    /* 
+        chalk.blue(), chalk.green(), chalk.red():
+        เป็นฟังก์ชันของ Chalk ที่ใช้กำหนดสีของข้อความ
+        chalk.blue() จะแสดงข้อความเป็นสีน้ำเงิน
+        chalk.green() จะแสดงข้อความเป็นสีเขียว
+        chalk.red() จะแสดงข้อความเป็นสีแดง
+        สามารถใช้สีอื่นๆ ได้อีกมาก เช่น chalk.yellow(), chalk.magenta(), chalk.cyan() เป็นต้น
+    */
   });
 
 program.parse(process.argv);
